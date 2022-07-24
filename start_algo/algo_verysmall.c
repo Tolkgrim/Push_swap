d3020df2fa18a1a0fea7be6aa3047ae6a4aaa6ca
Julien Dutschke <jdutschk@k0r1p13.42mulhouse.fr>
2022-07-24 20:15:36 +0200

    verysmall algo finish

--- start_algo/algo_verysmall.c
+++ start_algo/algo_verysmall.c
@@ -6,7 +6,7 @@
 /*   By: jdutschk <jdutschk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/15 13:43:37 by jdutschk          #+#    #+#             */
-/*   Updated: 2022/07/24 20:10:28 by jdutschk         ###   ########.fr       */
+/*   Updated: 2022/07/24 20:14:18 by jdutschk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,21 @@ void	algo_verysmall(int *stack_a, int *stack_b, int *index_a, int *index_b)
 	if (stack_a[0] > stack_a[1] && stack_a[1] < stack_a[2]
 		&& stack_a[0] < stack_a[2])
 		s_a(stack_a, stack_b, index_a, index_b);
-	if (stack_a[0] > stack_a[1] && stack_a[1] < stack_a[2]
+	else if (stack_a[0] > stack_a[1] && stack_a[1] < stack_a[2]
 		&& stack_a[0] > stack_a[2])
 		r_a(stack_a, stack_b, index_a, index_b);
-	if (stack_a[0] > stack_a[1] && stack_a[1] > stack_a[2]
+	else if (stack_a[0] > stack_a[1] && stack_a[1] > stack_a[2]
 		&& stack_a[0] > stack_a[2])
 	{
 		s_a(stack_a, stack_b, index_a, index_b);
 		r_ra(stack_a, stack_b, index_a, index_b);
-		p_b(stack_a, stack_b, index_a, index_b);
 	}
-	print_stack(stack_a, index_a, "loulou");
+	else if (stack_a[0] < stack_a[1] && stack_a[1] > stack_a[2]
+		&& stack_a[0] < stack_a[2])
+	{
+		s_a(stack_a, stack_b, index_a, index_b);
+		r_a(stack_a, stack_b, index_a, index_b);
+	}
+	else
+		r_ra(stack_a, stack_b, index_a, index_b);
 }
